1dacb09a5fa524065215119ce7db7f0c63598f6f
lindadijeh@gmail.com <lindadijeh@gmail.com>
2022-04-04 12:12:50 -0500

    deletd previous version of project

--- FoodTrackerCoreMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FoodTrackerCoreMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
