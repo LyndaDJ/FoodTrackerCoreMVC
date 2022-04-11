a23d5cd3be44b687a82cc73d9bad76195efe4faf
lindadijeh@gmail.com <lindadijeh@gmail.com>
2022-04-11 12:43:07 -0500

    Added controller action tests

--- FoodTrackerTests/FoodsControllerTests.cs
+++ FoodTrackerTests/FoodsControllerTests.cs
@@ -1,25 +1,26 @@
-using FoodTrackingApp.Controllers;
-using FoodTrackingApp.Models;
-using FoodTrackingApp.Repositories;
+using FoodTrackingApp2.Controllers;
+using FoodTrackingApp2.Models;
+using FoodTrackingApp2.Repositories;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace FoodTrackerTests
 {
     public class FoodsControllerTests
     {
-        private FoodsController foodsController;
-        private Mock<IFood> mockfoodRepo;
+        private FoodController foodsController;
+        private Mock<IFoodRepository> mockfoodRepo;
         private Food food, foodWith_id;
 
         [SetUp]
         public void Setup()
         {
-            mockfoodRepo = new Mock<IFood>();
-            foodsController = new FoodsController(mockfoodRepo.Object);
+            mockfoodRepo = new Mock<IFoodRepository>();
+            foodsController = new FoodController(mockfoodRepo.Object);
             foodWith_id = food = new Food
-            { 
-                Username = "lyn@gmail.com",
+            {
                 Snacks = "N/A",
                 Fat = "coconut oil",
                 Protein = "chicken",
@@ -31,33 +32,63 @@ namespace FoodTrackerTests
 
         }
         [Test]
-        public void Index_ReturnsViewWithListOfFoodRecords()
+        public void Index_ReturnsView_WithListOfFoodRecords()
         {
             //Arrange
 
+            IEnumerable<Food> foods = new[] { food, foodWith_id };
+            mockfoodRepo.Setup(m => m.GetAll()).Returns(foods);
+
             //Act
+            var result = foodsController.Index() as ViewResult;
 
             //Assert
-
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Model);
+            Assert.AreEqual(foods, result.Model);
         }
 
         [Test]
-        public void PostMethodCreate_InsertsFoodintoRecord()
+        public void Edit_ReturnsNotFound_WhenIdIs0()
         {
+            var result = foodsController.Edit(0);
+            Assert.That(result, Is.TypeOf<NotFoundResult>());
+        }
 
-            mockfoodRepo.Setup(repo => repo.InsertFoodRecord(food));
+        //[Test]
+        //public void Edit_ShouldReturnFoodWithSameID()
+        //{
+        //    var food = new Food() { Id = 1 };
+        //    mockfoodRepo.Setup(m => m.GetByID(1)).Returns(food);
 
+        //    var result = foodsController.Edit(id:1);
+
+        //    Assert.IsNotNull(result);
+        //    Assert.AreEqual(food.Id, result.C);
+        //}
+
+
+        [Test]
+        public void Delete_ReturnsView_WithFoodByID()
+        {
+            var food = new Food() { Id = 1 };
+            mockfoodRepo.Setup(repo => repo.GetByID(1)).Returns(food);
+
+            var result = foodsController.Delete(1) as ViewResult;
+
+            Assert.AreEqual(food, result.Model);
         }
 
         [Test]
-        public void SearchResults_ReturnsFilteredIndexbyPhrase()
+        public void DeletePOST_RedirectsToIndex()
         {
-            //Arrange
+            var food = new Food() { Id = 1 };
+            mockfoodRepo.Setup(repo => repo.GetByID(1)).Returns(food);
 
-            //Act
+            var result = foodsController.DeletePOST(food.Id) as RedirectToActionResult;
 
-            //Assert
-            Assert.Pass();
+            Assert.AreEqual("Index", result.ActionName);
         }
+
     }
 }
--- FoodTrackerTests/UserprofileControllerTests.cs
+++ FoodTrackerTests/UserprofileControllerTests.cs
@@ -1,4 +1,7 @@
-﻿using FoodTrackingApp.Controllers;
+﻿using FoodTrackingApp2.Controllers;
+using FoodTrackingApp2.Models;
+using FoodTrackingApp2.Repositories;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using NUnit.Framework;
 
@@ -6,21 +9,57 @@ namespace FoodTrackerTests
 {
     public class UserprofileControllerTests
     {
+        private ProfileController profileController;
+        private Mock<IProfileRepository> mockprofileRepo;
+        private Profile profile, profileWith_id;
         [SetUp]
         public void Setup()
         {
+            mockprofileRepo = new Mock<IProfileRepository>();
+            profileController = new ProfileController(mockprofileRepo.Object);
+            profileWith_id = profile = new Profile
+            {
+                Firstname ="Lynda",
+                Lastname = "Dijeh",
+                Height =   180,
+                Weight =   180,
+                Goalweight = 160
+            };
+            profileWith_id.Id = 1;
 
         }
 
         [Test]
-        public void Index()
+        public void Details_returnsUserProfilebyId()
         {
-            //Arrange
+            var profile = new Profile() { Id = 1 };
+            mockprofileRepo.Setup(repo => repo.GetByID(1)).Returns(profile);
 
-            //Act
+            var result = profileController.Details(1) as ViewResult;
 
-            //Assert
-            Assert.Pass();
+            Assert.AreEqual(profile, result.Model);
         }
+
+        [Test]
+        public void Details_ReturnsNotFound_WhenIdIs0()
+        {
+            //Profile nullprofile = null;
+            //mockprofileRepo.Setup(repo => repo.GetByID(0)).Returns(nullprofile);
+            
+            var result = profileController.Details(0);
+            Assert.That(result, Is.TypeOf<NotFoundResult>());
+        }
+
+        //[Test]
+        //public void Details_ReturnsNotFound_WhenProfileIsNull()
+        //{
+        //    Profile nullprofile = null;
+        //    mockprofileRepo.Setup(repo => repo.GetByID(0)).Returns(nullprofile);   
+
+
+        //    Assert.That(nullprofile., Is.TypeOf<NotFoundResult>());
+        //}
+
+
     }
 }
